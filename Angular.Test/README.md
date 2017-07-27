37d622b21db2f6af8af79b70da3134c612dc6cec
Ahmar Husain <ahusain3@asiapac.globalcsc.net>
2017-07-27 17:17:37 +0530

    Add project files.

---
+++ Angular.Test/README.md
@@ -0,0 +1,8 @@
+﻿# Angular.Test
+
+
+protractor conf.js
+webdriver-manager start
+
+http://www.protractortest.org/#/api?view=ElementFinder.prototype.isPresent
+https://hassantariqblog.wordpress.com/2015/11/06/installing-and-using-protractor-for-angularjs-application-in-visual-studio/
---
+++ Angular.Test/example_spec.js
@@ -0,0 +1,34 @@
+﻿describe('angularjs homepage title check', function () {
+    it('should have a title', function () {       
+        browser.get('https://signalrui.azurewebsites.net/#/');
+        expect(browser.getTitle()).toEqual('Signal R');
+    });
+});
+describe('Click Button', function () {
+    it('should have a button', function () {
+        var addButton = element(by.css('[value="Create"]')); 
+        addButton.click();        
+        expect(element(by.css('[id="Name"]')).isPresent()).toBe(true);
+    });
+});
+describe('Submit Button', function () {
+    it('Form should be Filled', function () {
+   
+        element(by.model('vm.AccountModel.Name')).sendKeys("Ahmar Husain");
+        element(by.model('vm.AccountModel.AccountNumber')).sendKeys("426124612");      
+        element(by.model('vm.AccountModel.Amount')).sendKeys("9000");
+        element(by.css('[class="btn btn-default"]')).click(); 
+        browser.driver.sleep(4000);
+        element(by.css('[class="btn btn-primary"]')).click();
+        browser.driver.sleep(4000);
+        expect(element(by.css('[value="Delete"]')).isDisplayed()).toBeTruthy();       
+    });    
+});
+describe('Delete Button', function () {
+    it('Data Should Be deleted', function () {   
+        element(by.css('[value="Delete"]')).click();
+        browser.driver.sleep(4000);
+        expect(element(by.css('[value="Delete"]')).isPresent()).toBe(false);
+        browser.pause();
+    });
+});
